3cafc284eb0fe7ecdd81acf0829c60f3ec48f9bf
Selvaganesh G <selvaganesh_g@photonint.com>
2019-06-14 19:45:00 +0530

    second commit

--- src/main/java/com/sg/SgApplication.java
+++ src/main/java/com/sg/SgApplication.java
@@ -4,7 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@RestController
 public class SgApplication {
+	
+	@GetMapping("/hello")
+    public String sayHello() {
+        return "Hello Selva you did it... !";
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(SgApplication.class, args);
